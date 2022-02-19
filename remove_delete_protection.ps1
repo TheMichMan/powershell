615c722f6eae3375d5ea00d869b741a4ca6d48f5
TheMichMan <76679322+TheMichMan@users.noreply.github.com>
2022-02-19 17:16:29 +0200

    Add files via upload

---
+++ remove_delete_protection.ps1
@@ -0,0 +1,7 @@
+﻿$OU = 'OU=People,DC=contoso,DC=local'
+
+$list = Get-ADOrganizationalUnit -SearchBase $OU -SearchScope Subtree -Filter * | Select-Object -ExpandProperty DistinguishedName
+foreach ($i in $list)
+{
+Get-ADOrganizationalUnit $i | Set-ADObject -ProtectedFromAccidentalDeletion:$false -Verbose
+}
